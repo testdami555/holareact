f6a4c7e13568c8dc7e79d3443b8183646871a625
testdami555 <testdami555@gmail.com>
2020-07-29 12:01:58 -0300

    Agregado nombre en index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const nombres = ["Dami","Pedro","Juana"];
+const nombres = ["Dami","Pedro","Juana","Arturo"];
 const Componente =()=> nombres.map((nombre,i)=><li key={i}>{i} - {nombre} ({nombre.length})</li>)
 ReactDOM.render(<ul><Componente /></ul>, document.getElementById('root'));
