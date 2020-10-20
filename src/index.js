36a608315c18d6bd424a7223f04e6848849185f2
testdami555 <testdami555@gmail.com>
2020-10-20 10:50:45 -0300

    Agregado Albert

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const nombres = ["Dami","Pedro","Juana","Arturo","Iris","Barry"];
+const nombres = ["Dami","Pedro","Juana","Arturo","Iris","Barry","Albert"];
 const Componente =()=> nombres.map((nombre,i)=><li key={i}>{i} - {nombre} ({nombre.length})</li>)
 ReactDOM.render(<ul><Componente /></ul>, document.getElementById('root'));
